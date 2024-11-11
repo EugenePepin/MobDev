89b7192977a29dfa37f92939e8026c818a1fd315
D0BR0D1Y <pepinevgen@gmail.com>
2024-11-11 15:24:09 +0200

    Lab1

---
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.lab1
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+            val textView: TextView = findViewById(R.id.textView)
+            val button: Button = findViewById(R.id.button)
+
+            button.setOnClickListener {
+                textView.text = "Пепін Євгеній К-400"
+            }
+        }
+
+    }
+
+
+
+
+
+
